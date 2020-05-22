f92023120c2d8d6c3ad6bbe216478a5ffaa293a5
interactive-directory-1839 <benthedunn@gmail.com>
2020-05-22 20:08:41 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -3,6 +3,9 @@ def find_max_value(array)
   high_val = -1
   while counter < array.length do
     if high_val < array[counter]
+      high_val = array[counter]
+    end
     count += 1
   end
+  high_val
 end
